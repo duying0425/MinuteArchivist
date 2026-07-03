077e801b91ca3e784f52ac947bdeb302a2943fcf
duyingfang <duyingfang@reachauto.com>
2026-07-03 14:34:19 +0800

    Change deploy.sh to use Tsinghua mirror and disable pip cache

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,9 @@ if [ ! -d ".venv" ]; then
 fi
 
 # 3. Upgrade pip and install dependencies
-echo "[+] 正在使用阿里云镜像安装/更新项目依赖包..."
-.venv/bin/python3 -m pip install --upgrade pip -i https://mirrors.aliyun.com/pypi/simple/
-.venv/bin/pip install -i https://mirrors.aliyun.com/pypi/simple/ -r requirements.txt
+echo "[+] 正在使用清华源镜像安装/更新项目依赖包..."
+.venv/bin/python3 -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
+.venv/bin/pip install --no-cache-dir -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 echo "[+] 依赖包安装完成。"
 
 # 4. Copy systemd user service configuration
